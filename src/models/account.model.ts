2f091ae4a56785bc5c3c003a0a1a8e36a567aec5
Sachin <121659364+sachinthapa572@users.noreply.github.com>
2025-09-27 18:04:01 +0545

    fix: cast ret to any before deleting refreshToken -- Signed-off-by: Sachin Thapa <contactsachin572@gmail.com>

--- src/models/account.model.ts
+++ src/models/account.model.ts
@@ -34,7 +34,7 @@ const accountSchema = new Schema<AccountDocument>(
     timestamps: true,
     toJSON: {
       transform(doc, ret) {
-        delete ret.refreshToken;
+        delete (ret as any).refreshToken;
       },
     },
   }
