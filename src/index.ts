40d9b0034e5164a047a2389fa60163b38c9ab658
Sachin <121659364+sachinthapa572@users.noreply.github.com>
2025-09-28 17:52:11 +0545

    style: standardize quotes and formatting in index.ts -- Signed-off-by: Sachin Thapa <contactsachin572@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import session from "cookie-session";
-import cors from "cors";
-import "dotenv/config";
-import express, { NextFunction, Request, Response } from "express";
-import { config } from "./config/app.config";
-import connectDatabase from "./config/database.config";
-import { ErrorCodeEnum } from "./enums/error-code.enum";
-import { asyncHandler } from "./middlewares/asyncHandler.middleware";
-import { errorHandler } from "./middlewares/errorHandler.middleware";
-import { BadRequestException } from "./utils/appError";
+import session from 'cookie-session';
+import cors from 'cors';
+import 'dotenv/config';
+import express, { NextFunction, Request, Response } from 'express';
+import { config } from './config/app.config';
+import connectDatabase from './config/database.config';
+import { ErrorCodeEnum } from './enums/error-code.enum';
+import { asyncHandler } from './middlewares/asyncHandler.middleware';
+import { errorHandler } from './middlewares/errorHandler.middleware';
+import { BadRequestException } from './utils/appError';
 
-import passport from "passport";
-import "./config/passport.config";
-import router from "./routes/router";
-import morgan from "morgan";
+import morgan from 'morgan';
+import passport from 'passport';
+import './config/passport.config';
+import router from './routes/router';
 
 const app = express();
 const BASE_PATH = config.BASE_PATH;
@@ -20,16 +20,18 @@ const BASE_PATH = config.BASE_PATH;
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 app.use(
   session({
-    name: "session",
+    name: 'session',
     keys: [config.SESSION_SECRET],
     maxAge: 24 * 60 * 60 * 1000,
-    secure: config.NODE_ENV === "production",
+    secure: config.NODE_ENV === 'production',
     httpOnly: true,
-    sameSite: "lax",
+    sameSite: config.NODE_ENV === 'production' ? 'none' : 'lax',
+    domain:
+      config.NODE_ENV === 'production' ? config.FRONTEND_ORIGIN : undefined,
   })
 );
 
@@ -47,7 +49,7 @@ app.get(
   `/`,
   asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     throw new BadRequestException(
-      "This is a bad request",
+      'This is a bad request',
       ErrorCodeEnum.AUTH_INVALID_TOKEN
     );
   })
